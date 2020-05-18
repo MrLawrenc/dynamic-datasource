6a4d614fb8c86e3c77949a617ea52969b0679379
刘明瑶-hz20035009 <liumingyao@huize.com>
2020-05-18 14:20:27 +0800

    feature:refactor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## dynamicdatasource模块为动态数据源示例项目
-## database-migration模块为数据库迁移项目
+## [dynamicdatasource模块为动态数据源示例项目](./dynamic-datasource-demo/README.md)
+## [database-migration模块为数据库迁移项目](./database-migration/README.md)
---
+++ database-migration/src/main/java/com/swust/dynamicdatabasemigration/DynamicDatabaseMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.swust.dynamicdatabasemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatabaseMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatabaseMigrationApplication.class, args);
+    }
+
+}
